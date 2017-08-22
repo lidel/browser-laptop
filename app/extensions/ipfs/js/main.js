08b4f38288fa0dabda1a602528d75bfdbce50903
David Dias <daviddias.p@gmail.com>
2017-08-22 18:41:25 +0100

    it loads an IPFS node!

--- app/extensions/ipfs/js/main.js
+++ app/extensions/ipfs/js/main.js
@@ -1,3 +1,5 @@
+/* global Ipfs */
+
 // const ipc = window.chrome.ipcRenderer
 
 chrome.protocol.registerStringProtocol('ipfs', handler)
@@ -5,14 +7,26 @@ chrome.protocol.registerStringProtocol('dweb', handler)
 
 function handler (request, callback) {
   // test to check if handling the protocol works
-  callback('hi there!' + test()) // eslint-disable-line
+  // callback('hi there!' + test()) // eslint-disable-line
 
-  /*
   const node = new Ipfs()
+
   node.on('ready', () => {
-    callback('hi there!' + test() + Ipfs) // eslint-disable-line
+    node.files.cat('QmSmuETUoXzh4Qo5upHxJWZJK8AEpXXZdTqs34ttE3qMYn', (err, stream) => {
+      if (err) {
+        return callback('failed to get the hash')
+      }
+      let buf = ''
+      stream.on('data', (data) => {
+        buf += data.toString()
+      })
+      stream.on('end', () => {
+        callback(buf)
+      })
+    })
+    // callback('I am online!') // eslint-disable-line
   })
-  */
+
   // test loading Ipfs into the background process scope
   // callback('hi there!' + test() + Ipfs.toString()) // eslint-disable-line
 }
